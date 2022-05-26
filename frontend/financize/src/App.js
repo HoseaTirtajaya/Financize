b7e89a0abf9a684959e9b111739459c592ff07c5
Hosea Tirtajaya <hoseatirtajaya@gmail.com>
2022-05-26 19:51:07 +0700

    Update little"

--- frontend/financize/src/App.js
+++ frontend/financize/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css"
 
-import store from "./store";
 import Login from "./components/Login";
 import Home from "./components/Home";
-
+  
 export default () => 
   <BrowserRouter>
       <Routes>
